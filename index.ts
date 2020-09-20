eb1f38bc6d940de1ef8f3fa4127c962365b506de
Daniel S <iam@w-b.dev>
2020-09-20 00:20:47 -0400

    add limit to img size. format img name. del images

--- index.ts
+++ index.ts
@@ -1,16 +1,22 @@
 import multer from "multer";
 import express from "express";
 import cors from "cors";
-import { imageFilter } from "./utils";
+import { imageFilter, getFormattedDate, cleanFolder } from "./utils";
 
 const UPLOAD_PATH = "uploads";
+cleanFolder(UPLOAD_PATH);
+
 const storage = multer.diskStorage({
   destination: `${UPLOAD_PATH}/`,
   filename: (req, file, cb) => {
-    cb(null, file.originalname.replace(/\s/g, ""));
+    cb(null, getFormattedDate() + "_" + file.originalname.replace(/\s/g, ""));
   },
 });
-const upload = multer({ storage, fileFilter: imageFilter });
+const upload = multer({
+  storage,
+  fileFilter: imageFilter,
+  limits: { fileSize: 100000 }, //100KB-ish
+});
 
 const app = express();
 app.use(cors());
@@ -19,9 +25,10 @@ app.listen(3000, () => console.log("Listening 3000"));
 // route
 app.post("/upload", upload.single("image"), async (req, res) => {
   try {
-    // console.log(req.file);
+    const formattedDate = getFormattedDate();
+    console.log(formattedDate, req.file);
     res.send({
-      id: Date.now(),
+      id: formattedDate,
       filename: req.file.originalname.replace(/\s/g, ""),
     });
   } catch (error) {
--- utils.ts
+++ utils.ts
@@ -1,4 +1,4 @@
-import { Multer } from "multer";
+import del from "del";
 
 export const imageFilter = function (req: any, file: any, cb: any) {
   // accept image only
@@ -6,4 +6,27 @@ export const imageFilter = function (req: any, file: any, cb: any) {
     return cb(new Error("Only image files are allowed!"), false);
   }
   cb(null, true);
+};
+
+export const getFormattedDate = () => {
+  const date = new Date(Date.now());
+  const rule = /[^A-Za-z0-9]/g;
+  const _2 = date
+    .toLocaleString("en-CA", {
+      timeZoneName: "short",
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+    })
+    .replace(rule, "_");
+  return _2;
+};
+
+export const cleanFolder = function (folderPath: string) {
+  // delete files inside folder but not the folder itself
+  del.sync([`${folderPath}/**`, `!${folderPath}`]);
 };
